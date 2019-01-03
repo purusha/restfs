caa650dfc94a4e692ef3de739b3e0c7202acfcff
Alan Toro <alan.toro@ext.contactlab.com>
2019-01-03 17:42:17 +0100

    for file model check also length attr

--- backend/src/test/java/it/at/restfs/PatternBuilder.java
+++ backend/src/test/java/it/at/restfs/PatternBuilder.java
@@ -2,6 +2,7 @@ package it.at.restfs;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import it.at.restfs.storage.AssetType;
 
 public class PatternBuilder {
 
@@ -9,23 +10,36 @@ public class PatternBuilder {
     private static final String J_S = "\\{";    
     private static final String J_E = "\\}";
 
-    public static String file(String f) {
-        return J_S + "\"created\"" + DT + ",\"lastAccess\"" + DT + ",\"length\":0,\"modified\"" + DT + ",\"name\":\"" + f + "\",\"type\":\"FILE\"" + J_E;        
+    public static String file(String name) {        
+        return file(name, 0l);
     }
     
-    public static String folder(String f, String... children) {
+    public static String file(String name, long length) {
+        return 
+            J_S + 
+            "\"created\"" + DT + "," +
+            "\"lastAccess\"" + DT + "," +
+            "\"length\":" + length + "," + 
+            "\"modified\"" + DT + "," +
+            "\"name\":\"" + name + "\"," +
+            "\"type\":\"" + AssetType.FILE + "\"" + 
+            J_E;                
+    }
+    
+    public static String folder(String name, String... children) {
         final String childrenValue = Arrays.stream(children).collect(Collectors.joining(", "));
-        final Long numberOf = Arrays.stream(children).count();
+        final Long length = Arrays.stream(children).count();
         
         return 
             J_S + 
             "\"children\":\\[" + childrenValue + "\\]," + 
             "\"created\"" + DT + "," + 
             "\"lastAccess\"" + DT + "," + 
-            "\"length\":" + numberOf + "," + 
+            "\"length\":" + length + "," + 
             "\"modified\"" + DT + "," + 
-            "\"name\":\"" + f + "\"," + 
-            "\"type\":\"FOLDER\"" + J_E;
+            "\"name\":\"" + name + "\"," + 
+            "\"type\":\"" + AssetType.FOLDER + "\"" + 
+            J_E;
     }
     
 }
--- backend/src/test/java/it/at/restfs/integration/Stage6.java
+++ backend/src/test/java/it/at/restfs/integration/Stage6.java
@@ -1,30 +1,36 @@
 package it.at.restfs.integration;
 
+import java.util.List;
 import org.junit.Test;
 import com.google.common.collect.Iterables;
 import it.at.restfs.BaseTest;
 import it.at.restfs.Operation;
 import okhttp3.ResponseBody;
 
+import static it.at.restfs.PatternBuilder.file;
+
 public class Stage6 extends BaseTest {  
+    
+    private static final String TEXT = "my body";
   
     @Test
     public void simpleCase() throws Exception {              
-        final ResponseBody open = Iterables.getLast(
-            runCommands(
-                ExecutionContext.builder()
-                    .container(getContainer())
-                    .stopOnError(true)
-                    .build(),
-                buildCommand("file", Operation.CREATE),
-                buildCommand("file", Operation.APPEND, "my body"),
-                buildCommand("file", Operation.OPEN)
-            )                
-        );
+        final List<ResponseBody> commands = runCommands(
+            ExecutionContext.builder()
+                .container(getContainer())
+                .stopOnError(true)
+                .build(),
+            buildCommand("file", Operation.CREATE),
+            buildCommand("file", Operation.APPEND, TEXT),
+            buildCommand("file", Operation.OPEN)
+        );                
+        
+        match(
+            file("file", TEXT.length()), Iterables.get(commands, 1).string()
+        );        
         
         expected(
-            "{\"content\":[\"my body\"],\"path\":\"/file\"}", 
-            open.string()
+            "{\"content\":[\"" + TEXT + "\"],\"path\":\"/file\"}", Iterables.get(commands, 2).string()
         );        
     }    
 
@@ -57,19 +63,19 @@ public class Stage6 extends BaseTest {
                     .stopOnError(true)
                     .build(),
                 buildCommand("file4", Operation.CREATE),
-                buildCommand("file4", Operation.APPEND, "my body"),
+                buildCommand("file4", Operation.APPEND, TEXT),
                 buildCommand("file4", Operation.APPEND, "\n"),
-                buildCommand("file4", Operation.APPEND, "my body"),
+                buildCommand("file4", Operation.APPEND, TEXT),
                 buildCommand("file4", Operation.APPEND, "\r"),
-                buildCommand("file4", Operation.APPEND, "my body"),
+                buildCommand("file4", Operation.APPEND, TEXT),
                 buildCommand("file4", Operation.APPEND, "\r\n"),
-                buildCommand("file4", Operation.APPEND, "my body"),
+                buildCommand("file4", Operation.APPEND, TEXT),
                 buildCommand("file4", Operation.OPEN)
             )                
         );
         
         expected(
-            "{\"content\":[\"my body\",\"my body\",\"my body\",\"my body\"],\"path\":\"/file4\"}", 
+            "{\"content\":[\"" + TEXT + "\",\"" + TEXT + "\",\"" + TEXT + "\",\"" + TEXT + "\"],\"path\":\"/file4\"}", 
             open.string()
         );        
     }    
